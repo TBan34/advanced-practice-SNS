6ea1dd731cf714e9416917bbc18002788641e589
TBan34 <119559612+TBan34@users.noreply.github.com>
2022-12-26 06:48:50 +0000

    [Update]コメント機能についてViewファイルへ記述、モデル名の修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,8 @@
-class PostCommentsController < ApplicationController
+class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(book_id: book.id)
+    comment = current_user.book_comments.new(book_id: book.id)
     comment.book_id = book.id
     comment.save
     redirect_back(fallback_location: root_path)
@@ -10,7 +10,7 @@ class PostCommentsController < ApplicationController
 
   def destroy
     book = Book.find(params[:book_id])
-    comment = current_user.post_comments.find_by(book_id: book.id)
+    comment = current_user.book_comments.find_by(book_id: book.id)
     comment.destroy
     redirect_back(fallback_location: root_path)
   end
