6da0323600c46ac9c1bd4e29201b3ee8b52f0d28
TBan34 <119559612+TBan34@users.noreply.github.com>
2022-12-31 02:20:05 +0000

    [Add]検索結果のビューファイルを作成、各ページのコード修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,17 @@
-class SearchsController < ApplicationController
+class SearchesController < ApplicationController
 
   before_action :authenticate_user!
 
   def search
     @range = params[:range]
+    @search_word = params[:word]
 
-    if @range == User
+    if @range == "User"
       @users = User.looks(params[:search], params[:word])
+      render "/searches/search_result"
     else
       @books = Book.looks(params[:search], params[:word])
+      render "/searches/search_result"
     end
   end
 
